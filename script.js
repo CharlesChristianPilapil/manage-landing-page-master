3ba22dd133b4b6d9568c32b405cd089f3ecfb936
CharlesChristianPilapil <charleschristian.pilapil@gmail.com>
2023-09-04 19:26:02 +0800

    update

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+const navBar = document.querySelector("#nav-bar");
+const navToggle = document.querySelector("#nav-toggle");
+
+navToggle.addEventListener("click", () => {
+  navToggle.classList.toggle("active");
+
+  if (navToggle.classList.contains("active")) {
+    navToggle.src = "../images/icon-close.svg";
+  } else {
+    navToggle.src = "../images/icon-hamburger.svg";
+  }
+
+  navBar.classList.toggle("active");
+});
+
+document.onclick = function(e) {
+  if(e.target !== navBar && e.target !== navToggle) {
+    navBar.classList.remove('active');
+    navToggle.classList.remove('active')
+    navToggle.src = "../images/icon-hamburger.svg";
+  }
+}
+
+//   if(navToggle.src.match('../images/icon-hamburger.svg')) {
+//     navToggle.src='../images/icon-close.svg'
+//   }
+
+//   else {
+//     navToggle.src='../images/icon-hamburger.svg'
+//   }
+// });
+
 $(document).ready(function () {
   $("#responsive").slick({
     dots: true,
@@ -24,7 +56,7 @@ $(document).ready(function () {
       {
         breakpoint: 768,
         settings: {
-        arrows: false,
+          arrows: false,
           slidesToShow: 1,
           slidesToScroll: 1,
         },
